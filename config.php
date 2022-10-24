7c57597073b8c4e137f67e2c311c2c1cbffc2689
BrunoOliveiraPR <116589076+BrunoOliveiraPR@users.noreply.github.com>
2022-10-24 15:20:25 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php 
+
+  
+
+    $SANDBOX_ENVIRONMENT = false;
+
+    
+    $JS_FILE_URL = "https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+    $PAGSEGURO_API_URL = 'https://ws.pagseguro.uol.com.br/v2';
+
+    $PAGSEGURO_EMAIL = 'helixcursos@gmail.com';
+    $PAGSEGURO_TOKEN = '#';
+
+
+
+?>
